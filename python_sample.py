b7319eff44f55a9eb1b694e30b7a65b91ec71865
dedetyan <37791671+dedetyan@users.noreply.github.com>
2019-04-23 17:14:18 +0900

    サンプル

---
+++ python_sample.py
@@ -0,0 +1,38 @@
+num1 = 5
+num2 = 7
+ans1 = num1 + num2
+ans2 = num1 - num2
+print(ans1,ans2)
+
+num1 = 8.3
+num2 = 3.2
+msg = '計算結果 ='
+result1 = num1 * num2
+result2 = num1 / num2
+print(msg, result1)
+print(msg, result2)
+
+x = 0
+print(x)
+x = x + 50
+print(x)
+x = x + 50
+print(x)
+x = x + 50
+print(x)
+
+msg3 = '底辺3 高さ6の三角形の面積は'
+a = 3
+h = 6
+s = (a * h) / 2
+print(msg3, s)
+
+msg4 = '底辺3.3 高さ6.5の三角形の面積は'
+a = 3.3
+h = 6.5
+s = (a * h) / 2
+print(msg4, s)
+
+t = 27
+θ = 9/5 * t + 32
+print('セ氏',t,'[°C]', '=', '華氏', θ ,'[°F]')
