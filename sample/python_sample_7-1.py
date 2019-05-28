dfaea2e0dc4f8b7409a64ccc5ca2e6bfe44cf313
dedetyan <37791671+dedetyan@users.noreply.github.com>
2019-05-28 22:12:54 +0900

    年齢を判別してメッセージを出力

---
+++ sample/python_sample_7-1.py
@@ -0,0 +1,11 @@
+age = int(input('age: '))
+
+if age >= 20 :
+        print('お酒を買わせる')
+
+else :
+        if age >= 0 :
+                print('お酒を買わせない')
+
+        else :
+                print('値が不適切です')
