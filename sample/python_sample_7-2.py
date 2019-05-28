6d8e8f3e07075c254b552897a0a060805f0af786
dedetyan <37791671+dedetyan@users.noreply.github.com>
2019-05-28 22:13:21 +0900

    ３つの値から最大値を表示する

---
+++ sample/python_sample_7-2.py
@@ -0,0 +1,17 @@
+a = int(input('a: '))
+b = int(input('b: '))
+c = int(input('c: '))
+
+if a > b :
+      if a > c :
+            print('最大値は:', a)
+
+      else :
+            print('最大値は:', c)
+
+else :
+      if b > c :
+           print('最大値は:', b)
+
+      else :
+           print('最大値は:', c)
