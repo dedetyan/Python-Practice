bcef2be5cfd7a77e03d68b9a33f4b247bcf77ec0
dedetyan <37791671+dedetyan@users.noreply.github.com>
2019-06-25 13:36:28 +0900

    更に短く

--- sample/python_sample_10-6.py
+++ sample/python_sample_10-6.py
@@ -1,15 +1,6 @@
 a = int(input('値を入力してください: '))
 b = int(input('値を入力してください: ')) #aはbよりも大きい値を入力する。
 
-if a % 2 == 0:
-    for k in range(a,b):
-        a = a + 1
-        if a % 2 != 0:
-            print('奇数は', a)
-
-else:
-    print('奇数は', a)
-    for k in range(a,b):
-        a = a + 1
-        if a % 2 != 0:
-            print('奇数は', a)
+for k in range(a,b):
+    if k % 2 != 0:
+        print('奇数は', k)
