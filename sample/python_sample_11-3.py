358785b8da595f795beb692eb0df2384a52e47b7
dedetyan <37791671+dedetyan@users.noreply.github.com>
2019-06-25 23:53:33 +0900

    入力した数字の素数判定
    改良の余地ありかも

---
+++ sample/python_sample_11-3.py
@@ -0,0 +1,12 @@
+n = int(input('自然数を入力してください:'))
+
+if n > 1:
+    for i in range(2,n):
+        if n % i == 0:
+            print(n, 'は素数でない')
+            break
+        else:
+            print(n, 'は素数です')
+
+else:
+    print(n, 'は素数でない')
