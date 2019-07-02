98df7e17388cb025d520c7ea3e7050e2f48b3cc5
dedetyan <37791671+dedetyan@users.noreply.github.com>
2019-07-02 13:20:22 +0900

    "素数です"が複数表示されないように。

--- sample/python_sample_11-3.py
+++ sample/python_sample_11-3.py
@@ -5,8 +5,12 @@ if n > 1:
         if n % i == 0:
             print(n, 'は素数でない')
             break
-        else:
-            print(n, 'は素数です')
+    else:
+        print(n, 'は素数です')
 
 else:
-    print(n, 'は素数でない')
+    if n == 1:
+        print(n, 'は素数でない')
+
+    else:
+        print(n, 'は不正な値です。')
